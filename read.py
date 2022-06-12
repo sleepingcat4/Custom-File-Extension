19b287bc0176a6903fc2c0a4f27000c8e05da195
TAWSIF AHMED <stanfordblaze4@gmail.com>
2022-06-12 18:20:02 +0600

    GEN 1

---
+++ read.py
@@ -0,0 +1,7 @@
+#reading the required file
+read = open('ammar.tawsif', 'r')
+
+#looping through the entire file sentences!
+for i in read:
+    print(i)
+read.close()
---
+++ write.py
@@ -0,0 +1,11 @@
+#creating a file with custom extension
+extension = open('ammar.tawsif', 'w')
+
+#writing something in the file
+extension.write("We're volunteer team who create software and research papers that's high-end")
+
+#Closing the file
+extension.close()
+
+#printing successful message
+print("Done successful!")
